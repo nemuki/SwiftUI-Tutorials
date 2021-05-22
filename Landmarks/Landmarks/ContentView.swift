c4d4b152b81f75cabb46d9633cb2131dc705fcc1
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-05-22 09:55:03 +0900

    Landmarks Senction 3が終わった

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -10,8 +10,20 @@ import SwiftUI
 // Viewプロトコルに準拠した、ビューのコンテンツとレイアウトの記述
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
-            .font(.title)
+        // Stackのデフォルトは中央配置。イニシャライザで変更できる。
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                // スペーサーのデフォルトは親の大きさ
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
 }
 

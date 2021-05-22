0ca46ce1f282ef0c7ce359f1c154a954dc66c194
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-05-22 09:16:52 +0900

    Initial Landmarks App

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by fullerene-06 on 2021/05/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
